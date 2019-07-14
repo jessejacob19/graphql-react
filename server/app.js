ab1758f7518da6c43a719e36a0f2a8dcae8a11c3
jesse collier <jesse.collier97@gmail.com>
2019-07-14 14:32:03 +1200

    added graphql endpoint without configuration

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const graphqlHTTP = require('express-graphql')
 
 const app = express();
+
+
+app.use('/graphql', graphqlHTTP({
+  
+}))
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`now listening on port ${port}`);

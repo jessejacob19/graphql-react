ad852f437a349e1863382456a4bb0bec15196931
jesse collier <jesse.collier97@gmail.com>
2019-07-14 17:30:13 +1200

    added book => author functionality

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,9 +8,9 @@ const {
 const _ = require("lodash");
 
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: '1' },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: '2' },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: '3' }
 ];
 
 var authors = [
@@ -24,7 +24,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, {id: parent.authorId})
+      }
+    }
   })
 });
 
@@ -51,7 +57,7 @@ const RootQuery = new GraphQLObjectType({
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
-      resolve(parents, args) {
+      resolve(parent, args) {
         return _.find(authors, { id: args.id });
       }
     }

31a67240ceaccd10ef539350bd4f7e1123a004bd
jesse collier <jesse.collier97@gmail.com>
2019-07-14 20:34:04 +1200

    connected to mongodb instance

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
-const graphqlHTTP = require('express-graphql')
-const schema = require('./schema/schema')
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect(
+  "mongodb+srv://jesse:Oscarian1664@cluster0-yr8ib.mongodb.net/test?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to the database");
+});
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
